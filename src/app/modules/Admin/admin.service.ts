835661ff805dec35022b665ecfa7b6bec9542c41
orun-sunny <sunny.mukut@gmail.com>
2025-03-02 23:10:21 +0600

    search optimize

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,8 +3,11 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 const getAllFromDB = async (params: any) => {
+    const {searchTerm, ...filterData} = params; 
     const andConditions: Prisma.AdminWhereInput[] = [];
     const adminSearchAbleFielelds = ['name', 'email'];
+
+    console.log(filterData);    
 
     if (params.searchTerm) {
         andConditions.push(
@@ -17,7 +20,19 @@ const getAllFromDB = async (params: any) => {
                 }))
             })
 
+    };
+
+    if(Object.keys(filterData).length > 0){
+        andConditions.push({
+            AND:Object.keys(filterData).map(key=>({
+                [key]:{
+                    equals:filterData[key]
+                }
+
+            }))
+        })
     }
+
     console.dir(andConditions, { depth: Infinity });
     const whereConditions = { AND: andConditions }
     const result = await prisma.admin.findMany({
